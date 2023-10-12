740affcc5770483c1ba0e5002e7711fe32e2c5c6
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-10-12 22:33:43 +0530

    finished Navigation drawer structure and styling

--- js/global.js
+++ js/global.js
@@ -7,6 +7,7 @@
 
 // Imports
 import { ripple } from "./utils/ripple.js";
+import { addEventOnElements } from "./utils/event.js"
 
 // Header on-scroll state
 const /** {NodeElement} */ $header = document.querySelector("[data-header]");
@@ -19,13 +20,26 @@ window.addEventListener("scroll", () => {
  * Add ripple effect
  */
 const /** {NodeList} */ $rippleElems =
-document.querySelectorAll("[data-ripple]");
+    document.querySelectorAll("[data-ripple]");
 
 $rippleElems.forEach(($rippleElem) => ripple($rippleElem));
 
 
 
 /**
+ * Navbar toggle for mobile screens
+ */
+const /** NODELIST */ $navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const /** NODEELEMENT */ $navbar = document.querySelector("[data-navigation]");
+const /** NODEELEMENT */ $scrim = document.querySelector("[data-scrim]");
+
+addEventOnElements($navTogglers, "click", function () {
+    $navbar.classList.toggle("show");
+    $scrim.classList.toggle("active");
+});
+
+
+/**
  * Filter functionality
  */
-window.filterObj ={};
+window.filterObj = {};
