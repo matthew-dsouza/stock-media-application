13795f9ab9b1229491a874f27d4ce016724473bd
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-10-24 16:30:15 +0530

    popular videos section - scaffolding and styling

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@
  */
 import { client } from "./api_configure.js";
 import { photoCard } from "./photo_card.js";
+import { gridInit, updateGrid } from "./utils/masonry_grid.js";
 
 /**
  * Render curated photos in home page
@@ -19,13 +20,18 @@ const /** NodeElement */ $photoGrid =
 
 $photoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
 
-client.photos.curated({ page: 1, per_page: 5 }, (data) => {
+client.photos.curated({ page: 1, per_page: 20 }, (data) => {
   $photoGrid.innerHTML = "";
   const /** Object */ photoGrid = gridInit($photoGrid);
 
   data.photos.forEach((photo) => {
     const /** NodeElement */ $photoCard = photoCard(photo);
 
-    $photoGrid.appendChild($photoCard);
+    updateGrid($photoCard, photoGrid.columnsHeight, photoGrid.$columns);
   });
 });
+
+
+/** 
+ * Render popular videos in home page
+ */
